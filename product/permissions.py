f7c2258b84118641f97d0e4a9b408af1ad01a6e0
Zmac <zmac@Zmacs-MacBook-Pro.local>
2025-07-29 00:12:40 +0530

    recipt dowmload

--- product/permissions.py
+++ product/permissions.py
@@ -2,7 +2,7 @@ from rest_framework import permissions
 
 class IsOwnerOrAdmin(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj.created_by == request.user or request.user.is_staff
+        return request.user.is_superuser is True
 
 class IsCustomerOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
